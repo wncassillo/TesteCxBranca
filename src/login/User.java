9eef789bf17fef0f746aed5d8b3ee960c1b8acfe
wncassillo <cassilloneto@gmail.com>
2023-10-04 16:57:49 -0300

    Update Contando Partes do Código

--- src/login/User.java
+++ src/login/User.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class User {
     //ponto de Início
-    public Connection conectarBD(){ //Ponto 02 tentando conexão com bd
+    public Connection conectarBD(){ //Ponto 02: tentando conexão com bd
         Connection conn = null;
         try{
             Class.forName("com.mysql.Driver.Manager").newInstance();
@@ -15,16 +15,18 @@ public class User {
             conn =  DriverManager.getConnection(url);
         }catch (Exception e) { }
         return conn;}
-    //Ponto 03 retorno da conexão
-    //Ponto Final 01: Falha na conexão ao BD
+    //Ponto 03: retorno da conexão
+    //Ponto 05: sucesso na conexão (continua para o 6) OU
+    //Ponto 04: Falha na conexão ao BD e então:
+    //Ponto 13: FIM.   
 
-    //Ponto 1 Variaveis de controle de login
+    //Ponto 1: Variaveis de controle de login
     //1 pq não está dentro de um método, roda quando uma isntancia da classe é criada.
     public String nome="";
     public boolean result = false;
     
     public boolean verificarUsuario(String login, String senha){
-        //Ponto 04 entrando com informações pra pegar dados no BD
+        //Ponto 06: entrando com informações pra pegar dados no BD
         String sql = "";
         Connection conn = conectarBD();
         //INSTRUÇÃO SQL
@@ -34,16 +36,20 @@ public class User {
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            //Ponto 05 Retorno dos dados do BD
+            //Ponto 07: Retorno dos dados do BD
             
-            //Ponto 06 IF Verificando resultados
+            //Ponto 08: IF Verificando resultados
+            //Ponto 10: Consulta com restultados (continua para o 11)
+            //Ponto 09: Consulta sem resultados e então
+            //Ponto 13: Fim 
             if(rs.next()){ //Ponto final 02: Falha na consulta
                 
-                //Ponto 07 "Update" variaves de controle
+                //Ponto 11: "Update" variaves de controle
                 result = true;
                 nome = rs.getString("nome");}
             }catch (Exception e) { } 
-            //Ponto 08 return da variavel result.
+            //Ponto 12 return da variavel result.
+            //Ponto 13 Fim, mas dessa vez com sucesso.
             return result;} 
             //Ponto Final 03: Sucesso!
         }//fim da class
