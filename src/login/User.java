104ba0c5946d5f1ff0e49448f0c491ec9d4c3ffe
wncassillo <cassilloneto@gmail.com>
2023-10-04 14:48:15 -0300

    Contando Pontos no Código

--- src/login/User.java
+++ src/login/User.java
@@ -6,7 +6,8 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class User {
-    public Connection conectarBD(){
+    //ponto de Início
+    public Connection conectarBD(){ //Ponto 02 tentando conexão com bd
         Connection conn = null;
         try{
             Class.forName("com.mysql.Driver.Manager").newInstance();
@@ -14,9 +15,16 @@ public class User {
             conn =  DriverManager.getConnection(url);
         }catch (Exception e) { }
         return conn;}
+    //Ponto 03 retorno da conexão
+    //Ponto Final 01: Falha na conexão ao BD
+
+    //Ponto 1 Variaveis de controle de login
+    //1 pq não está dentro de um método, roda quando uma isntancia da classe é criada.
     public String nome="";
     public boolean result = false;
+    
     public boolean verificarUsuario(String login, String senha){
+        //Ponto 04 entrando com informações pra pegar dados no BD
         String sql = "";
         Connection conn = conectarBD();
         //INSTRUÇÃO SQL
@@ -26,9 +34,16 @@ public class User {
         try{
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery(sql);
-            if(rs.next()){
+            //Ponto 05 Retorno dos dados do BD
+            
+            //Ponto 06 IF Verificando resultados
+            if(rs.next()){ //Ponto final 02: Falha na consulta
+                
+                //Ponto 07 "Update" variaves de controle
                 result = true;
                 nome = rs.getString("nome");}
-            }catch (Exception e) { }
+            }catch (Exception e) { } 
+            //Ponto 08 return da variavel result.
             return result;} 
+            //Ponto Final 03: Sucesso!
         }//fim da class
