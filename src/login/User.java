60ff7be15b137ec7cd44ffe1ee97cfc3d6f7e809
wncassillo <cassilloneto@gmail.com>
2023-10-14 14:06:50 -0300

    Documentação da Classe

--- src/login/User.java
+++ src/login/User.java
@@ -5,51 +5,55 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+/** Classe User é responsável por conectar ao BD,
+ * validar Login do Usuário 
+ * e guardar o nome do mesmo. **/
 public class User {
-    //ponto de Início
-    public Connection conectarBD(){ //Ponto 02: tentando conexão com bd
+    /**Método conectarBD: 
+     * Responsável por criar uma coenxão com o bd
+     * @return o método retorna a conexão, para ser usada em outros métodos. **/
+    public Connection conectarBD(){ 
         Connection conn = null;
         try{
             Class.forName("com.mysql.Driver.Manager").newInstance();
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
             conn =  DriverManager.getConnection(url);
         }catch (Exception e) { }
-        return conn;}
-    //Ponto 03: retorno da conexão
-    //Ponto 05: sucesso na conexão (continua para o 6) OU
-    //Ponto 04: Falha na conexão ao BD e então:
-    //Ponto 13: FIM.   
-
-    //Ponto 1: Variaveis de controle de login
-    //1 pq não está dentro de um método, roda quando uma isntancia da classe é criada.
-    public String nome="";
-    public boolean result = false;
+        return conn;} 
     
+    /** As variaveis da classe são definidas e inicializadas. **/
+    public String nome="";
+    /** A variavel nome guardará o nome do usuario,
+     * caso este consiga fazer login.**/
+    public boolean result = false;
+    /** A variavel booleana result demarcar se o usuário fez login ou não.
+     * O valor da mesma se altera para True caso o login seja bem-sucedido.**/
+    
+    /**Método verificarUsuario: 
+     * Esse método é o responsável por fazer a consulta MYSQL 
+     * com o login e senha informados pelo usuario, para então determinar
+     * se o login foi bem-sucedido ou não.
+     * @return valor booleano que indica se o login foi bem-sucedido ou não.**/
     public boolean verificarUsuario(String login, String senha){
-        //Ponto 06: entrando com informações pra pegar dados no BD
         String sql = "";
         Connection conn = conectarBD();
         //INSTRUÇÃO SQL
+        /**Concatenação de String predefinida + login e senha
+         * inseridos pelo usuário, gerando a linha de sql que
+         realizará a consulta.**/
         sql += "select nome from usuarios ";
         sql +="where login = " + "'" + login + "'";
         sql += " and senha = " + "'" + senha + "';";
         try{
             Statement st = conn.createStatement();
-            ResultSet rs = st.executeQuery(sql);
-            //Ponto 07: Retorno dos dados do BD
-            
-            //Ponto 08: IF Verificando resultados
-            //Ponto 10: Consulta com restultados (continua para o 11)
-            //Ponto 09: Consulta sem resultados e então
-            //Ponto 13: Fim 
-            if(rs.next()){ //Ponto final 02: Falha na consulta
-                
-                //Ponto 11: "Update" variaves de controle
+            ResultSet rs = st.executeQuery(sql);  
+            /** IF verifica se houve um retorno da consulta realizada
+             * Em caso negativo, é lançado um erro.
+             * Em caso positivo o código continua, onde as variaveis
+             * result e nome recebem novos valores.**/
+            if(rs.next()){    
                 result = true;
                 nome = rs.getString("nome");}
             }catch (Exception e) { } 
-            //Ponto 12 return da variavel result.
-            //Ponto 13 Fim, mas dessa vez com sucesso.
             return result;} 
-            //Ponto Final 03: Sucesso!
         }//fim da class
